ae927f595610a730becb65cec581e11a2429a251
liwei <liwei5917@gmail.com>
2016-12-30 16:00:53 +0800

    impletement group#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,12 +7,16 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def create
     @group = Group.new(group_params)
     @group.save
 
     redirect_to groups_path
-  end 
+  end
 
     private
 
